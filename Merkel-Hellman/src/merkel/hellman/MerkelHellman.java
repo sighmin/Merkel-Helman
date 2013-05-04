0a1131d29527aa5ed22810be3e032d4c5a2a3039
Simon van Dyk <simon.vandyk@gmail.com>
2013-05-04 09:22:39 +0200

    init

---
+++ Merkel-Hellman/src/merkel/hellman/MerkelHellman.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package merkel.hellman;
+
+/**
+ *
+ * @author simon
+ */
+public class MerkelHellman {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
